b38f93cea534449e8fd35cfce1a0f41a793d9898
RatsovPhilip <ratsovphilip@gmail.com>
2019-06-15 01:29:26 +0300

    Added packages services

---
+++ src/Apps/Panda/Panda.Services/Packages/IPackagesService.cs
@@ -0,0 +1,15 @@
+﻿using Panda.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Panda.Services.Packages
+{
+    public interface IPackagesService
+    {
+        void Create(string description, decimal weight, string shippingAdrres, string recipientName);
+
+        IQueryable<Package> GetAllByStatus(PackageStatus status);
+    }
+}
---
+++ src/Apps/Panda/Panda.Services/Packages/PackagesService.cs
@@ -0,0 +1,43 @@
+﻿using Panda.Data;
+using Panda.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Panda.Services.Packages
+{
+    public class PackagesService : IPackagesService
+    {
+        private readonly PandaDbContext context;
+
+        public PackagesService(PandaDbContext context)
+        {
+            this.context = context;
+        }
+
+        public void Create(string description, decimal weight, string shippingAdrres, string recipientName)
+        {
+            var userId = this.context.Users.Where(x => x.UserName == recipientName)
+                .Select(u => u.Id).FirstOrDefault();
+
+            var package = new Package
+            {
+                Description = description,
+                Weight = weight,
+                Status = PackageStatus.Pending,
+                ShippingAddress = shippingAdrres,
+                RecipientId = userId
+            };
+
+            this.context.Packages.Add(package);
+            this.context.SaveChanges();
+        }
+
+        public IQueryable<Package> GetAllByStatus(PackageStatus status)
+        {
+            var packages = this.context.Packages.Where(x => x.Status == status);
+            return packages;
+        }
+    }
+}
